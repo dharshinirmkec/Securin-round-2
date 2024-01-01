9169f3b539c3caa18510a0bda38aceabff7608ab
dharshinirmkec <90687862+dharshinirmkec@users.noreply.github.com>
2024-01-01 20:07:22 +0530

    Add files via upload

---
+++ dice-1.py
@@ -0,0 +1,6 @@
+#Calculating the probability
+die_no_a = [1,2,3,4,5,6]
+die_no_b = [1,2,3,4,5,6]
+
+combinations_total = len(die_no_a) * len(die_no_b)
+print("The total possibilities of combinations include: ", combinations_total)
