86f3e18ea5c2347b275f05cec2c56d3b9c95522b
dharshinirmkec <90687862+dharshinirmkec@users.noreply.github.com>
2024-01-01 20:12:39 +0530

    Add files via upload

---
+++ dice-3.py
@@ -0,0 +1,19 @@
+die_no_a = [1,2,2,3,3,4]
+die_no_b = [1,3,4,5,6,8]
+
+sum_combinations = {}
+
+for i in die_no_a:
+    for j in die_no_b:
+        if (i+j) in sum_combinations:
+            sum_combinations[i+j] += 1
+        else:
+            sum_combinations[i+j] = 1
+
+print(sorted(sum_combinations))
+
+"""
+print("Enter the sum of values of two dices: ", end="")
+sum = int(input())
+print("Number of combinations to get the sum", sum, "is", combinations_sum[sum], "/36")    
+"""
